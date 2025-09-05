20dc189ba35ec4b586dc887327070a285795cc49
AScaravilli <AScaravilli@B1842ZACP0323>
2025-09-05 12:03:55 -0300

    Verificar si la app ya esa en uso

--- AccesosLauncher/App.xaml.cs
+++ AccesosLauncher/App.xaml.cs
@@ -1,15 +1,14 @@
 using Microsoft.Extensions.Configuration;
 using System.IO;
+using System.Threading;
 using System.Windows;
-using Application = System.Windows.Application;
 
 namespace AccesosLauncher
 {
-    public partial class App : Application
+    public partial class App : System.Windows.Application
     {
-        //public static IConfiguration Configuration { get; private set; }
         public static IConfiguration Configuration { get; private set; } = null!;
-
+        private static Mutex? _mutex;
 
         public App()
         {
@@ -18,6 +17,22 @@ namespace AccesosLauncher
                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
 
             Configuration = builder.Build();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            const string appName = "AccesosLauncher_Mutex_2d5e7c1e-9b2c-4a6a-9b6d-2f3e7c8d9a1b";
+            
+            _mutex = new Mutex(true, appName, out bool createdNew);
+
+            if (!createdNew)
+            {
+                System.Windows.MessageBox.Show("AccesosLauncher ya se está ejecutando.", "Aplicación en ejecución", MessageBoxButton.OK, MessageBoxImage.Information);
+                System.Windows.Application.Current.Shutdown();
+                return;
+            }
+
+            base.OnStartup(e);
         }
     }
 }
